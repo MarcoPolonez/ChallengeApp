eeb660479cbe3720cc9779fb61b931067326f619
MarcoPolonez <marek.karci@gmail.com>
2023-03-02 16:36:05 +0000

    4 dzien

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -3,10 +3,14 @@ var myAge = 33;
 bool mezczyzna = false;
 
 if (mezczyzna == false)
+{
     if (myAge < 30 && myName == "Ewa")
         Console.WriteLine("Mloda kobieta");
     else if (myName == "Ewa" && myAge == 33)
         Console.WriteLine("Ewa ma 33 lat");
+}
 if (mezczyzna == true)
+{
     if (myAge < 18)
         Console.WriteLine("Nieletni mezczyzna");
+}
