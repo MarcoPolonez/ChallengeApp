e1c3cae16ba05c26c83c1e42f75e65875a02afcf
MarcoPolonez <marek.karci@gmail.com>
2023-03-02 15:49:49 +0000

    zadanie domowe dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string myName = "Marek";
+var myAge = 13;
+bool mezczyzna = true;
+
+
+if (myAge > 18 && mezczyzna == true)
+    Console.WriteLine("Facet jest stary dziad");
+
+if (myAge < 18 && mezczyzna == true)
+    Console.WriteLine("Niepelnoletni mezczyzna");
+
+if (myName == "Marek" && myAge == 33)
+    Console.WriteLine("Marek to 33 latek");
